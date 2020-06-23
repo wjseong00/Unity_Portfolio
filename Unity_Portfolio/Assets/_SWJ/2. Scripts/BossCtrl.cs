12e097d60d5e193a8ec16a05d7938d95084654ad
wonjin0217 <yssung021@gmail.com>
2020-06-23 18:22:10 +0900

    보스 패턴 수정
    - 보스 벼락 패턴 재구현
    - 보스 화염 발사 구현
    - 보스 토네이도 미완
    - 유니티버전 : Unity 2019.3.14f1 (64-bit)
    - Visual Studio 버전 : Visual Community 2017

--- Unity_Portfolio/Assets/_SWJ/2. Scripts/BossCtrl.cs
+++ Unity_Portfolio/Assets/_SWJ/2. Scripts/BossCtrl.cs
@@ -1,73 +1,92 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BossCtrl : MonoBehaviour
 {
     Transform player;
-    Rigidbody rig;
+    CharacterController cc;
+    public Transform firePos;
     enum AttackPattern
     {
-        idle,fly,laser
+        idle,fly,laser,tornado
     }
     AttackPattern state;
     float speed = 1f;
     bool dash = false;
-    Vector3 upLocate;
-    Vector3 origin;
+
+    Animator anim;
+
     #region "썬더공격에 대한 함수"
-    public GameObject Circle;
+    
     public GameObject Thunder;
     public GameObject ThunderTarget;
-    GameObject cir;
+    
     bool makeCircle=false;
     float curTime = 0;
     float AttTime = 1f;
     float curTimeChange = 0f;
-    float changeTime = 6f;
+    float changeTime = 7f;
+    float upTime = 0f;
+    float DownTime = 0f;
+    float fullTime = 2.5f;
     Vector3 target;
     Vector3 preTarget;
+    private Shake shake;
     #endregion
-    #region "레이저공격에 대한 함수"
+    #region "파이어공격에 대한 함수"
     bool traceTarget = false;
-    public GameObject Laser;
+   
     bool makeLaser = false;
-    GameObject laser;
-    float laserTime = 0f;
-    float locateTime = 0f;
-    float updateLaser = 0.4f;
-    float updateLocate = 0.2f;
-    Vector3 preTar;
-    Transform preRot;
+    public GameObject blastFactory;
+     GameObject blast;
+    float fireTime = 0f;
+    float usingFire = 5f;
 
     #endregion
     #region "회오리공격에 대한 함수"
-
+    public GameObject TornadoFactory;
+    GameObject tornado1;
+    GameObject tornado2;
+    float speedT = 1.5f;
+    float speed2T = 1.0f;
+    public Transform torLocate1;
+    public Transform torLocate2;
+    bool creTor = false;
     #endregion
 
     void Start()
     {
         player = GameObject.Find("Player").transform;
         state = AttackPattern.idle;
-        rig = GetComponent<Rigidbody>();
-        
-    }
+        cc = GetComponent<CharacterController>();
+        blast = Instantiate(blastFactory);
+        blast.SetActive(false);
+        anim= GetComponent<Animator>();
+        shake = GameObject.Find("CameraRig").GetComponent<Shake>();
+        tornado1 = Instantiate(TornadoFactory);
+        tornado1.SetActive(false);
+        tornado2 = Instantiate(TornadoFactory);
+        tornado2.SetActive(false);
 
+    }
     
-    
+
+
     private void Update()
     {
         
         switch (state)
         {
             case AttackPattern.idle:
-                Idle();
+                
                 break;
             case AttackPattern.fly:
                 FlyAttack();
                 break;
-            
+            case AttackPattern.tornado:
+                TornadoAttack();
+                break;
             
             case AttackPattern.laser:
                 LaserAttack();
@@ -77,146 +96,168 @@ public class BossCtrl : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.T))
         {
-            origin = transform.position;
-            upLocate = transform.position + new Vector3(0, 5, 0);
-            curTime = 0f;
-            curTimeChange = 0f;
+            ValueReset();
             state =AttackPattern.fly;
         }
         if (Input.GetKeyDown(KeyCode.Y))
         {
-            
+            ValueReset();
             state = AttackPattern.laser;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            ValueReset();
+            state = AttackPattern.tornado;
         }
     }
 
-    private void Idle()
+    
+    
+    void ValueReset()
     {
+        upTime = 0f;
+        DownTime = 0f;
         
+        curTime = 0f;
+        curTimeChange = 0f;
     }
-
     //날아서 돌진공격
     private void FlyAttack()
     {
-        
+        Vector3 dir1 = (player.position - transform.position).normalized;
+        dir1.y = 0;
+        transform.rotation = Quaternion.LookRotation(dir1);
         if(curTimeChange > changeTime)
         {
-            rig.useGravity = true;
-            makeCircle = false;
-            Destroy(cir);
-            if (Vector3.Distance(transform.position, origin) < 0.1f)
+            anim.SetBool("FlyAttack", false);
+           
+            cc.enabled = true;
+            DownTime += Time.deltaTime;
+            
+            if (DownTime<fullTime)
             {
                 
-                state = AttackPattern.laser;
+                transform.Translate(Vector3.down * 2 * Time.deltaTime);
+            }
+            else
+            {
+                ValueReset();
+               
+                anim.SetBool("FireAttack",true);
+                state = (AttackPattern)Random.Range(1,4);
                 Debug.Log("바뀜");
-                
             }
             
         }
         else
         {
-            rig.useGravity = false;
-        }
-        
-        transform.position = Vector3.Lerp(transform.position, upLocate, speed * Time.deltaTime);
-        if(Vector3.Distance(transform.position,upLocate)<0.1f)
-        {
-            
-            if (!makeCircle)
+            upTime += Time.deltaTime;
+            Vector3 dir = (player.position - transform.position).normalized;
+            if (upTime < fullTime)
             {
-                cir = Instantiate(Circle);
-                cir.transform.position = transform.position - new Vector3(0,4.47f,0);
-                makeCircle = true;
-                
-            }
-
-            
-            
-        }
-        if(makeCircle)
-        {
-            curTimeChange += Time.deltaTime;
-            Collider[] cols = Physics.OverlapSphere(transform.position - new Vector3(0, 4.47f, 0), 5f);
-            if(cols.Length<=0)
-            {
-                Debug.Log("타겟이 없음");
+                transform.Translate(Vector3.up * 2 * Time.deltaTime);
             }
             else
             {
-                for (int i = 0; i < cols.Length; i++)
+                cc.enabled = false;
+                
+                curTimeChange += Time.deltaTime;
+                
+                Collider[] cols = Physics.OverlapSphere(transform.position - new Vector3(0, 3.5f, 0), 10f);
+                if (cols.Length <= 0)
                 {
-                    if(cols[i].tag == "Player")
+                    Debug.Log("타겟이 없음");
+                }
+                else
+                {
+                    for (int i = 0; i < cols.Length; i++)
                     {
-                        target = cols[i].transform.position;
-                        
-                        curTime += Time.deltaTime;
-                        if (curTime > AttTime)
+                        if (cols[i].tag == "Player")
                         {
-                            curTime = 0f;
-                            GameObject thunT = Instantiate(ThunderTarget);
-                            thunT.transform.position = target + new Vector3(0, 0.4f, 0);
-                            preTarget = thunT.transform.position;
-                            Destroy(thunT, 0.7f);
-                            Invoke("ThunderAttack", 0.7f);
+                            
+                            target = cols[i].transform.position;
+
+                            curTime += Time.deltaTime;
+                            if (curTime > AttTime)
+                            {
+                                anim.SetBool("FlyAttack", true);
+                                curTime = 0f;
+                                GameObject thunT = Instantiate(ThunderTarget);
+                                thunT.transform.position = target + new Vector3(0, 0.3f, 0);
+                                preTarget = thunT.transform.position;
+                                Destroy(thunT, 0.7f);
+                                Invoke("ThunderAttack", 0.7f);
+                            }
+                            else
+                            {
+                                anim.SetBool("FlyAttack", false);
+                            }
                         }
                     }
+
                 }
-                
             }
         }
-
-
-
+        
     }
+    
     void ThunderAttack()
     {
+        
+        StartCoroutine(shake.ShakeCamera());
         GameObject Thun = Instantiate(Thunder);
         Thun.transform.position = preTarget;
         Destroy(Thun, 0.5f);
     }
-    void trace()
-    {
-        Vector3 dir = player.position - transform.position;
-        transform.rotation = Quaternion.LookRotation(dir);
-        preTar = transform.position;
-        preRot = transform;
-    }
-
-    private void LateUpdate()
-    {
-        if(traceTarget)
-        {
-            
-            if (!makeLaser)
-            {
-                laser = Instantiate(Laser);
-                laser.transform.up = transform.forward;
-                makeLaser = true;
-            }
-            laser.transform.position = transform.position;
-            laser.transform.up = Vector3.Lerp(laser.transform.up, transform.forward, 40 * Time.deltaTime);
-        }
-        
-    }
+    
+    
     //추적레이저
     private void LaserAttack()
     {
-        traceTarget = true;
-        laserTime += Time.deltaTime;
-        Invoke("trace", 0.3f);
-        if(laserTime>updateLaser)
-        {
-            laserTime = 0f;
-            
-            
-            
-        }
-        
+        anim.SetBool("FireAttack",true);
+        Vector3 dir = (player.position - firePos.position).normalized;
+        dir.y = 0;
+        blast.SetActive(true);
 
+        blast.transform.position = firePos.position;
+        blast.transform.rotation = Quaternion.LookRotation(dir);
+        
+        transform.rotation = Quaternion.LookRotation(dir);
+        if (Vector3.Distance(transform.position,player.position)>5f)
+        {
+            transform.Translate(Vector3.forward * 1.5f * Time.deltaTime);
+        }
+        fireTime += Time.deltaTime;
+        if(fireTime>usingFire)
+        {
+            fireTime = 0f;
+            anim.SetBool("FireAttack", false);
+            blast.SetActive(false);
+            ValueReset();
+            state = (AttackPattern)Random.Range(1, 4);
+        }
         //laser.transform.rotation = transform.rotation;
     }
+    private void TornadoAttack()
+    {
+        Invoke("createTornado", 2f);
 
-    
+        tornado1.transform.position = Vector3.Lerp(tornado1.transform.position, player.position, speedT * Time.deltaTime);
+        tornado2.transform.position = Vector3.Lerp(tornado2.transform.position, player.position, speed2T * Time.deltaTime);
+    }
+    void createTornado()
+    {
+        tornado1.SetActive(true);
+        tornado2.SetActive(true);
+        if(!creTor)
+        {
+            tornado1.transform.position = torLocate1.position;
+            tornado2.transform.position = torLocate2.position;
+            creTor = true;
+        }
+        
+        
+    }
 
-    
+
 }
